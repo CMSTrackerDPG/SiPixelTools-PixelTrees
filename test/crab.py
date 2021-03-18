e3bc49a6390887fda7e5d37bf698bcd0625050ea
caleb-james-smith <caleb_smith2@baylor.edu>
2021-03-18 16:31:18 -0500

    prepare for v3 submission

--- test/crab.py
+++ test/crab.py
@@ -4,11 +4,13 @@ import glob
 from WMCore.Configuration import Configuration
 config = Configuration()
 
+# To submit crab jobs:
+# crab submit -c <config_file>
+
 config.section_('General')
 config.General.transferOutputs = True
 config.General.transferLogs = True
-#config.General.requestName = 'PixelTree_SingleMuon_2018C_Part1_RAW_v2'
-config.General.requestName = 'PixelTree_SingleMuon_2018C_Part2_RAW_v2'
+config.General.requestName = 'PixelTree_SingleMuon_2018C_Part1_RAW_v3'
 
 config.section_('JobType')
 config.JobType.allowUndistributedCMSSW = True
@@ -20,24 +22,28 @@ config.JobType.maxJobRuntimeMin = 3000
 config.JobType.maxMemoryMB = 4000
 
 # Run2 2018C: Run 319337 to 320191
-# Diff. in run num: 320191 - 319337 = 854, 854/2 = 427
-# Middle run: 319337 + 427 = 319764
+# Diff. in run num: 320191 - 319337 = 854
+# Split into groups with range of 200 runs
+# Part1: Run 319337 to 319536
 
 config.section_('Data')
 config.Data.inputDataset = '/SingleMuon/Run2018C-v1/RAW'
-#config.Data.runRange = '319337-319764' # 2018C Part1
-config.Data.runRange = '319765-320191' # 2018C Part2
+config.Data.runRange = '319337-319536' # 2018C Part1
 
 # put slash at end of path!
-#config.Data.outLFNDirBase = '/store/user/<your-location>'
+#config.Data.outLFNDirBase = '/store/user/<CERN-username>/<output-directory>'
 config.Data.outLFNDirBase = '/store/user/caleb/PixelTrees/'
-config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'FileBased'
+config.Data.splitting = 'Automatic'
 config.Data.unitsPerJob = 1
 config.Data.publication = True
 config.Data.ignoreLocality = True
 
+# To check crab write permissions: 
+# crab checkwrite --site <site>
+
 # Sites: T2_US_Nebraska, T3_US_FNALLPC
 config.section_('Site')
-config.Site.storageSite = 'T3_US_FNALLPC' # make sure you have write access here
+config.Site.storageSite = 'T2_US_Nebraska' # make sure you have write access here
 config.Site.whitelist = ['T2_DE_DESY','T2_FR_IPHC','T2_CH_CERN','T2_IT_Bari','T1_IT_*','T2_US_*','T3_US_*']
 
