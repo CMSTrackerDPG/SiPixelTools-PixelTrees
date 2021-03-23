67d9f78baf0d05a09c1107814cde8f6e2cd9d388
Kim Jalosjos <kimj@t3.unl.edu>
2021-03-23 11:16:30 -0500

    modified the crab file

--- test/crab.py
+++ test/crab.py
@@ -7,8 +7,8 @@ config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
 config.General.transferLogs = True
-#config.General.requestName = 'PixelTree_SingleMuon_2018C_Part1_RAW_v1'
-config.General.requestName = 'PixelTree_SingleMuon_2018C_Part2_RAW_v1'
+config.General.requestName = 'PixelTree_SingleMuon_2018C_Part1_RAW_v1'
+#config.General.requestName = 'PixelTree_SingleMuon_2018C_Part2_RAW_v1'
 
 config.section_('JobType')
 config.JobType.allowUndistributedCMSSW = True
@@ -24,13 +24,13 @@ config.Data.inputDataset = '/SingleMuon/Run2018C-v1/RAW'
 # Run2 2018C: Run 319337 to 320191
 # Diff. in run num: 320191 - 319337 = 854, 854/2 = 427
 # Middle run: 319337 + 427 = 319764
-#config.Data.runRange = '319337-319764' # 2018C Part1
-config.Data.runRange = '319765-320191' # 2018C Part2
+config.Data.runRange = '319337-319764' # 2018C Part1
+#config.Data.runRange = '319765-320191' # 2018C Part2
 #config.Data.runRange = '315257-316995' # only process these runs
 # put slash at end of path!
 #config.Data.outLFNDirBase = '/store/user/<your-location>'
 #config.Data.outLFNDirBase = '/store/user/calebsmith/PixelTrees/'
-config.Data.outLFNDirBase = '/store/user/caleb/PixelTrees/'
+config.Data.outLFNDirBase = '/mnt/hadoop/user/uscms01/pnfs/unl.edu/data4/cms/store/user/kimj/PixelTrees/'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.publication = True
@@ -38,5 +38,5 @@ config.Data.ignoreLocality = True
 
 # T3_US_Kansas, T2_US_Nebraska, T3_US_FNALLPC
 config.section_('Site')
-config.Site.storageSite = 'T3_US_FNALLPC' # or anything where you have access to
+config.Site.storageSite = 'T3_US_Kansas' # or anything where you have access to
 config.Site.whitelist = ['T2_DE_DESY','T2_FR_IPHC','T2_CH_CERN','T2_IT_Bari','T1_IT_*','T2_US_*']
